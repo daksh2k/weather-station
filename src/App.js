8a4a982bb0328bc9cc93fa9ab2d529fa153f1b36
rushik <rushikgohel@gmail.com>
2021-11-26 19:00:58 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {useState,useEffect} from 'react';
 import moment from 'moment';
 // import database from './firebase';
 import { WiHumidity,WiRaindrops } from "react-icons/wi";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { alignPropType } from 'react-bootstrap/esm/types';
 
 class App extends React.Component {
   constructor(props){
@@ -49,19 +52,39 @@ const Current = (props) => {
     <div className="curr">
         <div className="main">
           <div className="date-city text-start">
-            <div className="city fw-bold">Delhi,Delhi,India</div>
+            <div className="city fw-bold">Vadodara,Gujarat,India</div>
             <div className="time">{date +" | " +time}</div>
           </div>
            <div className="temp-cont">
               <div className="temperature-big">{props.cond.temp}</div>
               <sup>o</sup>
               <div className="unit">{props.cond.unit}</div>
+              <div className="cond text-end">
+                <div className="sec"><WiRaindrops />Rainfall: {props.cond.rain}mm</div>
+                <div className="sec"><WiHumidity />Humidity: {props.cond.humidity}%</div>
+              </div>
            </div>
         </div>
-        <div className="cond text-end">
+        {/*<div className="cond text-end">
+            <div className="sec"><WiRaindrops />Alkapuri: {props.cond.rain}mm</div>
+            <div className="sec"><WiHumidity />OP Road: {props.cond.humidity}%</div>
+  </div>*/}
+        {/*<div className="cond text-end">
             <div className="sec"><WiRaindrops />Rainfall: {props.cond.rain}mm</div>
             <div className="sec"><WiHumidity />Humidity: {props.cond.humidity}%</div>
-        </div>
+        </div>*/}
+        <Card style={{ width: '6rem' , height: '6rem'}}>
+            <Card.Body>
+              <Card.Title ><p style = {{fontSize: '0.6em'}}>Alkapuri</p></Card.Title>
+              <Card.Text>
+              <div className="temperature-sml" style = {{fontSize: '0.7em' }}>{props.cond.temp}</div>
+              <sup>o</sup>
+              <div className="unit">{props.cond.unit}</div>
+              <div className="sec" style = {{fontSize: '0.5em' }}><WiRaindrops />Rainfall: {props.cond.rain}mm</div>
+              <div className="sec" style = {{fontSize: '0.5em' }}><WiHumidity />Humidity: {props.cond.humidity}%</div>
+              </Card.Text>
+            </Card.Body>
+        </Card>
     </div>
   )
 } 
